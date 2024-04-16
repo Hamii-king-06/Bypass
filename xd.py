340f5688f4007ce12dfde822b646523eb9dfb3f9
HAMID MEER <85035495+Hamii-king-06@users.noreply.github.com>
2024-04-16 11:56:45 +0500

    Create xd.py

---
+++ xd.py
@@ -0,0 +1,42 @@
+import os
+
+Black= "\033[0;30m"
+Red= "\033[0;31m"
+Green = "\033[0;32m"
+Yellow = "\033[0;33m"
+Blue = "\033[0;34m"
+Magenta = "\033[0;35m"
+Cyan = "\033[0;36m"
+White = "\033[0;37m",
+Black = "\033[1;30m"
+Red = "\033[1;31m"
+Green = "\033[1;32m"
+Yellow = "\033[1;33m"
+Blue = "\033[1;34m"
+Magenta = "\033[1;35m"
+Cyan = "\033[1;36m"
+White = "\033[1;37m"
+Reset = "\033[0m"
+
+logo =f"""
+{White} _______           _______  _         
+(  ____ \|\     /|(  ____ \| \    /\  
+| (    \/| )   ( || (    \/|  \  / /  
+{Green}| (__    | |   | || |      |  (_/ /   
+|  __)   | |   | || |      |   _ (    
+{Magenta}| (      | |   | || |      |  ( \ \   
+| )      | (___) || (____/\|  /  \ \  
+{Reset}|/       (_______)(_______/|_/    \/  
+---------------------------------------------------------
+[>] Owner : Hamid Khawaja
+[>] challenge Gone Fucked By Hamii
+
+"""
+
+
+def fuck():
+    print(logo)
+    print(57*"-")
+    os.system("python3 Phuck_by_zeeshi.py")
+    exit()
+fuck()
